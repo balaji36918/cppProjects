22311d7a4d3c7461ba881f5d6aabbc58ceeda101
Balaji Venkatasubramanian <34167559+balaji36918@users.noreply.github.com>
2022-01-09 17:31:08 +0100

    Update House.cpp

--- cppdevUdemy/separateCompilation/House.cpp
+++ cppdevUdemy/separateCompilation/House.cpp
@@ -1,1 +1,47 @@
+#include "House.h"
+#include <iostream>
+using namespace std;
+
+//setters cant have constants as it cant be changed
+//::dcope resolution operator 
+void House::setNumStories(int numStories)
+{
+    //this -> since parameter name and datamember are same, this-> avoids overshadowing 
+    this->numStories = numStories;
+}
+
+void House::setNumWindows(int numWindows)
+{
+    this->numWindows = numWindows;
+}
+
+void House::setColor(string color) {
+    this->color = color;
+
+}
+//using keyword const is a considered as a good sw development practice 
+//the code will still run even after the removal of the term const
+//const is versatile - one s constant second s the function has access but the value cant be changed
+int House::getNumStories() const
+{
+    return numStories;
+}
+
+int House::getNumWindows() const
+{
+    return numWindows;
+
+}
+
+string House::getColor() const
+{
+    return color;
+}
+
+void House::print() const
+{
+    cout << "The house is " << color << " and has "
+        << numStories << " stories and "
+        << numWindows << "windows." << endl;
+}
 
