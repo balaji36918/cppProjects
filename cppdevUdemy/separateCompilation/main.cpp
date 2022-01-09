7b10ac40ccb944b407ec29ed02f0b744777c2e69
Balaji Venkatasubramanian <34167559+balaji36918@users.noreply.github.com>
2022-01-09 17:32:13 +0100

    Update main.cpp

--- cppdevUdemy/separateCompilation/main.cpp
+++ cppdevUdemy/separateCompilation/main.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <string>
+#include "House.h"
+
+using namespace std;
+
+void printHouseData(House house);
+void print();
+
+int main() {
+    House myHouse;
+    House yourHouse;
+
+    myHouse.setNumStories(2);
+    myHouse.setNumWindows(6);
+    myHouse.setColor("red");
+
+    yourHouse.setNumStories(3);
+    yourHouse.setNumWindows(10);
+    yourHouse.setColor("blue");
+
+    /*printHouseData(myHouse);
+    printHouseData(yourHouse);*/
+    myHouse.print();
+    yourHouse.print();
+    return 0;
+}
+
+void printHouseData(const House& house) {
+
+    cout << "The house is " << house.getColor() << " and has "
+        << house.getNumStories() << " stories and "
+        << house.getNumWindows() << "windows." << endl;
+}
 
