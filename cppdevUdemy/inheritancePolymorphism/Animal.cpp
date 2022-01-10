4310ee9dd571e65ec9299389f9b32d326d1df6df
Balaji Venkatasubramanian <34167559+balaji36918@users.noreply.github.com>
2022-01-10 21:15:26 +0100

    Create Animal.cpp

---
+++ cppdevUdemy/inheritancePolymorphism/Animal.cpp
@@ -0,0 +1,29 @@
+#include "Animal.h"
+
+Animal::Animal(string name, double weight)
+{
+	this->name = name;
+	this->weight = weight;
+}
+
+string Animal::getName() const
+{
+	return name;
+}
+
+double Animal::getWeight() const
+{
+	return weight;
+}
+
+void Animal::setName(string Name)
+{
+	this->name = name;
+}
+
+void Animal::setWeight(double weight)
+{
+	this->weight = weight;
+
+}
+
